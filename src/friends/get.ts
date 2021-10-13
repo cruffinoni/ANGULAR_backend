6f84fdc7822dbb922eff01c64d5465373c88ce2a
Clement SCHMITTE <clement.schmitte@epitech.eu>
2021-10-13 23:19:27 +0200

    feat/friendsList changed get for friends to get more data

--- src/friends/get.ts
+++ src/friends/get.ts
@@ -22,16 +22,21 @@ export class GetFriends implements Route {
         }
         const friends = friendships
           .filter((friend: Friends) => friend.status !== Friendship.DECLINED)
-          .map((friend: Friends) => {
-            return {
-              friendId:
-                friend.author == res.locals.authorization.id
-                  ? friend.userId
-                  : friend.author,
-              status: friend.status,
-            };
-          });
-        return formatReturnedMessage(res, httpCode.OK, friends);
+          
+        const result = await Promise.all(friends.map(async (friend: Friends) => {
+          const userProfile = await router.getDatabase.getUserById(friend.author == res.locals.authorization.id ? friend.userId : friend.author);
+          return {
+            friendId:
+              friend.author == res.locals.authorization.id
+                ? friend.userId
+                : friend.author,
+            toAccept: !(friend.author == res.locals.authorization.id),
+            pseudo: userProfile && userProfile.pseudo ? userProfile.pseudo : undefined,
+            mmr: userProfile && userProfile.mmr ? userProfile.mmr : undefined,
+            status: friend.status,
+          };
+        }))
+        return formatReturnedMessage(res, httpCode.OK, result);
       } catch (e) {
         console.error("[error - get friends]", e);
         return formatInternalServerErrorMessage(res);
